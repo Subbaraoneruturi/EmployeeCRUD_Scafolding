28c085a7424114ffb8aa058f66aa0b78f434f8a4
subas <subash.neruturi@gmail.com>
2022-10-07 19:56:34 +0530

    Create Update Delete and Details all are done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,12 +8,68 @@ namespace EmployeeCRUD_Scafolding.Controllers
 {
     public class HomeController : Controller
     {
+        TutorialsCS _context = new TutorialsCS();
         public ActionResult Index()
         {
-            TutorialsCS _context = new TutorialsCS();
-
             var listofData = _context.Employees.ToList();
             return View(listofData);
         }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee model)
+        {
+            _context.Employees.Add(model);
+            _context.SaveChanges();
+            var listofData = _context.Employees.ToList();
+            ViewBag.Message = "Data Insert Successfully";
+            return View("Index",listofData);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee Model)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == Model.EmployeeId).FirstOrDefault();
+            if (data != null)
+            {
+                data.EmployeeCity = Model.EmployeeCity;
+                data.EmployeeName = Model.EmployeeName;
+                data.EmployeeSalary = Model.EmployeeSalary;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+            _context.Employees.Remove(data);
+            _context.SaveChanges();
+            ViewBag.Messsage = "Record Delete Successfully";
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var data = _context.Employees.Where(x => x.EmployeeId == id).FirstOrDefault();
+            return View(data);
+        }
+
+
     }
 }
